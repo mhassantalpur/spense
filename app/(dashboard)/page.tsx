0441b940deb462ae474f0bff69931934b943979d
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-09 17:46:54 -0500

    styling: created welcome message component for user in header

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,6 @@
-import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-    <UserButton afterSwitchSessionUrl="/" />
+    <div>Dashboard Page</div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HeaderLogo from './header-logo'
 import Navigation from './navigation'
+import { UserButton, ClerkLoading, ClerkLoaded } from '@clerk/nextjs'
+import { Loader2 } from 'lucide-react'
+import WelcomeMsg from './welcome-msg'
 
 const Header = () => {
   return (
@@ -11,9 +14,15 @@ const Header = () => {
                     <HeaderLogo />
                     <Navigation />
                 </div>
+                <ClerkLoaded>
+                    <UserButton afterSwitchSessionUrl='/'/>
+                </ClerkLoaded>
+                <ClerkLoading>
+                    <Loader2 className='size-8 animate-spin text-slate-400' />
+                </ClerkLoading>
             </div>
+            <WelcomeMsg />
         </div>
-        Header
     </header>
   )
 }
