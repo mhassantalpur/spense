7aed9872f33ed3a8d9e8c98508b96a76abcd98ec
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-09 16:00:17 -0500

    feature: header component init

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,13 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import authors from './authors'
-import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api') // init new hono app with base path to be api
 
-// get authors and books route data
-app.route('/authors', authors);
-app.route('/books', books);
+app.get("/hello", (c) => {
+    return c.json({hello: "world"});
+})
 
 export const GET = handle(app) 
 export const POST = handle(app)
