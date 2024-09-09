3ab54a21ddb3ea68df795dfec793468778c72c4e
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-08 20:00:56 -0500

    styling: sign-in tailwind styling and loader added

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import { SignIn } from '@clerk/nextjs';
+import { Loader2 } from 'lucide-react';
+import { SignIn, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
 
 const SignInPage = () => {
   return (
-    <SignIn />
+    <div className='min-h-screen grid grid-cols-1 lg:grid-cols-2'>
+      <div className='h-full lg:flex flex-col items-center justify-center px-4'>
+        <div className='text-center space-y-4 pt-16'>
+          <h1 className='font-bold text-3xl text-muted-foreground text-[#2e2a47]'>
+            Welcome Back
+          </h1>
+          <p className='text-base text-[#7e8ca0]'>
+            Log in to resume your finacne journey
+          </p>
+        </div>
+        <div className='flex items-center justify-center mt-8'>
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className='animate-spin text-muted-foreground' />
+          </ClerkLoading>
+        </div>
+      </div>
+    </div>
   )
 }
 
