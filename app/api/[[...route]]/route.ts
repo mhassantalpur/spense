4fc868dcba2446503f3500618a3145f27597c266
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-09 15:16:03 -0500

    feature: user authentication from clerk db and hono test

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 
 export const runtime = 'edge'
 
@@ -9,16 +10,17 @@ const app = new Hono().basePath('/api') // init new hono app with base path to b
 
 // init hello get route to return context - context being the json message in this example
 app
-    .get('/hello', (c) => {
+    .get(
+        '/hello',
+        clerkMiddleware(),
+        (c) => {
+            const auth = getAuth(c);
+            if (!auth?.userId) {
+                return c.json({ error: "Unauthorized"})
+            }
         return c.json({
             message: 'Hello Next.js!',
-        })
-    })
-    .get('hello/:test', (c) => {
-        const test = c.req.param("test");
-        return c.json({
-            message: 'howdy',
-            test: test,
+            userId: auth.userId
         })
     })
 
