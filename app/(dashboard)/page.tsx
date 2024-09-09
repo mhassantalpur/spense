849661978325026431fee76c7a13770cb0d2dca3
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-08 19:16:41 -0500

    routing: dashboard created as root page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <Button variant={"destructive"}>click me</Button>
+    <p>this is an authenticated route</p>
   )
 }
