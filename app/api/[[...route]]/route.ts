65a7c39dc9cc4dcda8139b0e54d69f3c5365b94e
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-09 15:25:59 -0500

    feature: hono api route file structure

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,15 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api') // init new hono app with base path to be api
 
-// init hello get route to return context - context being the json message in this example
-app
-    .get(
-        '/hello',
-        clerkMiddleware(),
-        (c) => {
-            const auth = getAuth(c);
-            if (!auth?.userId) {
-                return c.json({ error: "Unauthorized"})
-            }
-        return c.json({
-            message: 'Hello Next.js!',
-            userId: auth.userId
-        })
-    })
+// get authors and books route data
+app.route('/authors', authors);
+app.route('/books', books);
 
 export const GET = handle(app) 
 export const POST = handle(app)
