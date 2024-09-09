fa53ca3515ed1bca980d147996151410f110e3c4
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-09 16:39:42 -0500

    feature: navigiation added to header with potential routes

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,8 +1,35 @@
 "use client";
 
+const routes = [
+    {
+        href: "/",
+        label: "Overview"
+    },
+    {
+        href: "/transactions",
+        label: "Transactions"
+    },
+    {
+        href: "/accounts",
+        label: "Accounts"
+    },
+    {
+        href: "/categories",
+        label: "Categories"
+    },
+    {
+        href: "/settings",
+        label: "Settings"
+    },
+]
+
 const navigation = () => {
   return (
-    <div>navigation</div>
+    <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
+        {routes.map((route) => (
+            <p>{route.label}</p>
+        ))}
+    </nav>
   )
 }
 
