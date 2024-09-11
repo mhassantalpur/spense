159c1651782616b59e24eb0996af348b0f0a654b
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-11 16:47:30 -0500

    feature: error handler updated in hono rpc route handler to account for standardized error if not a validation error

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,10 +1,18 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from "./accounts"
+import { HTTPException } from 'hono/http-exception'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api') // init new hono app with base path to be api
+
+app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+        return err.getResponse();
+    }
+    return c.json({error: "Internal error"} , 500)
+})
 
 const routes = app
     .route("/accounts", accounts);
