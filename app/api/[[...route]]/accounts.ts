053b3d0490ac5a3815c78d4d5ea1e4563be1f883
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-11 16:43:18 -0500

    feature: user validation added with clerk middleware for hono rpc to check if user exists and error handler created

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,15 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { HTTPException } from "hono/http-exception";
 
 const app = new Hono().
-    get("/", async (c) => {
+    get("/", clerkMiddleware(), async (c) => {
+        const auth = getAuth(c);
+        if (!auth?.userId) {
+            throw new HTTPException(401, {res: c.json({error: "unauthorized"}, 401)})
+        }
         const data = await db
             .select({
                 id: accounts.id,
