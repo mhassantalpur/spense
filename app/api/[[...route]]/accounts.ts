135686a2dfcff78502b8b0cc1377f164cfea6153
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-11 15:34:03 -0500

    feature: hono rpc handling for api routes

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,9 +1,8 @@
 import { Hono } from "hono";
 
-const app = new Hono();
-
-app.get("/", (c) => {
-    return c.json({accounts: []})
-})
+const app = new Hono().
+    get("/", (c) => {
+        return c.json({accounts: []})
+    })
 
 export default app;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -6,12 +6,10 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api') // init new hono app with base path to be api
 
-app.get("/hello", (c) => {
-    return c.json({hello: "world"});
-})
-
 const routes = app
     .route("/accounts", accounts);
 
 export const GET = handle(app) 
 export const POST = handle(app)
+
+export type AppType = typeof routes;
