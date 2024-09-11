be4bd9604b03aa1a011558927a952636eb286e4b
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-11 16:17:12 -0500

    testing: react query accounts custom hook inside dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,18 @@
 
+"use client"
+
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
+
 export default function Home() {
+  const accountsQuery = useGetAccounts()
   return (
-    <div>Dashboard Page</div>
+    <div>
+      {accountsQuery.data?.map((account) => (
+        <div key={account.id}>
+          {account.name}
+        </div>
+      ))}
+    </div>
   )
 }
