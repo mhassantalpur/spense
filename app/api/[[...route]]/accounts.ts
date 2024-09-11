28788e5f9e1518a157bf22a884589969b393d053
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-11 18:35:47 -0500

    feature: cuid init for post request values specifically distinct user id

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,33 +1,44 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
-import { accounts } from "@/db/schema";
+import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { HTTPException } from "hono/http-exception";
 import { eq } from "drizzle-orm";
+import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono().
-    get("/", clerkMiddleware(), async (c) => {
-        const auth = getAuth(c);
-        if (!auth?.userId) {
-            throw new HTTPException(401, {res: c.json({error: "unauthorized"}, 401)})
-        }
-        const data = await db
-            .select({
-                id: accounts.id,
-                name: accounts.name 
-            })
-            .from(accounts)
-            .where(eq(accounts.userId, auth.userId));
+    get("/", clerkMiddleware(), 
+        async (c) => {
+            const auth = getAuth(c);
+            if (!auth?.userId) {
+                throw new HTTPException(401, {res: c.json({error: "unauthorized"}, 401)})
+            }
+            const data = await db
+                .select({
+                    id: accounts.id,
+                    name: accounts.name 
+                })
+                .from(accounts)
+                .where(eq(accounts.userId, auth.userId));
 
-        return c.json({data})
+            return c.json({data})
     })
-    .post("/", clerkMiddleware(), async (c) => {
-        const auth = getAuth(c);
-        if (!auth?.userId) {
-            throw new HTTPException(401, {res: c.json({error: "unauthorized"}, 401)})
-        }
+    .post("/", clerkMiddleware(), zValidator('json', insertAccountSchema.pick({name:true})), 
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid("json");
 
-        return c.json({})
+            if (!auth?.userId) {
+                throw new HTTPException(401, {res: c.json({error: "unauthorized"}, 401)})
+            }
+
+            const data = await db.insert(accounts).values({
+                id: "test",
+                userId: auth.userId,
+                ...values,
+            })
+
+            return c.json({})
     })
 
 export default app;
