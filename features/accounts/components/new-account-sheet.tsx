8a9bd669fc2a124992b652b6fdd605206863b4df
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-12 19:09:53 -0500

    styling: form sheet styling update

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,12 +1,13 @@
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet"
+import { AccountForm } from "./account-form";
 
 export const NewAccountSheet = () => {
     const { isOpen, onClose } = useNewAccount();
     return (
         <Sheet open={isOpen} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
+            <SheetContent className="bg-white space-y-4">
+                <SheetHeader className="text-black">
                     <SheetTitle>
                         New Account
                         <SheetDescription>
@@ -14,6 +15,7 @@ export const NewAccountSheet = () => {
                         </SheetDescription>
                     </SheetTitle>
                 </SheetHeader>
+                <AccountForm onSubmit={() => {}} disabled={false}/>
             </SheetContent>
         </Sheet>
     )
