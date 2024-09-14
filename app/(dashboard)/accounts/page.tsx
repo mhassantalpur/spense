f71357fe517746d13250a8e7a74255374bab4a46
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-13 19:45:52 -0500

    feature: accounts data table skeleton init and tested with shadcn test data provided

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,14 +1,40 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card" 
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
+import { Plus } from "lucide-react"
+
+import { Payment, columns } from "./columns"
+import { DataTable } from "@/components/data-table";
+
+const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
 
 const AccountsPage = () => {
+    const newAccount = useNewAccount();
     return (
-        <div>
-            <Card>
-                <CardHeader>
-                    <CardTitle>
+        <div className="max-w-screen-4xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
                         Accounts Page
                     </CardTitle>
+                    <Button onClick={newAccount.onOpen} size="sm">
+                        <Plus className="size-4 mr-2" />
+                        Add new
+                    </Button>
                 </CardHeader>
+                <CardContent>
+                    <DataTable columns={columns} data={data} />
+                </CardContent>
             </Card>
         </div>
     )
