edd38a50cb1d4f583baae641c5fa73c635615df9
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-15 15:27:29 -0500

    feature: delete api route created with custom hook feature to call delete route

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -3,9 +3,10 @@ import { db } from "@/db/drizzle";
 import { accounts, insertAccountSchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { HTTPException } from "hono/http-exception";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
 
 const app = new Hono().
     get("/", clerkMiddleware(), 
@@ -23,7 +24,8 @@ const app = new Hono().
                 .where(eq(accounts.userId, auth.userId));
 
             return c.json({data})
-    })
+        }
+    )
     .post("/", clerkMiddleware(), zValidator('json', insertAccountSchema.pick({name:true})), 
         async (c) => {
             const auth = getAuth(c);
@@ -40,6 +42,28 @@ const app = new Hono().
             }).returning();
 
             return c.json({ data });
-    })
+        }
+    )
+    .post("/bulk-delete", clerkMiddleware(), zValidator('json', z.object({ids: z.array(z.string())})),
+        async (c) => {
+            const auth = getAuth(c);
+            const values = c.req.valid('json');
+
+            if (!auth?.userId) {
+                throw new HTTPException(401, {res: c.json({error: "unauthorized"}, 401)})
+            }
+
+            const [data] = await db
+                .delete(accounts)
+                .where(
+                    and(
+                        eq(accounts.userId, auth.userId),
+                        inArray(accounts.id, values.ids)
+                    )
+                )
+                .returning({id: accounts.id})
+            return c.json({data}) 
+        },
+    )
 
 export default app;
