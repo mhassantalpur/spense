c8f154a1e6dd33726a59e657bbdfcd72e9f68dc8
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-15 14:40:26 -0500

    styling: loading state and loader added to accounts route

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,18 +3,34 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card" 
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
-import { Plus } from "lucide-react"
+import { Loader2, Plus } from "lucide-react"
 
 import { columns } from "@/app/(dashboard)/accounts/columns"
 import { DataTable } from "@/components/data-table";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts"
+import { Skeleton } from "@/components/ui/skeleton"
 
 const AccountsPage = () => {
     const accountsQuery = useGetAccounts();
     const accounts = accountsQuery.data || [];
     const newAccount = useNewAccount();
 
-    
+    if (accountsQuery.isLoading) {
+        return (
+            <div className="max-w-screen-4xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                    <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                        <Skeleton className="h-8 w-48" />
+                    </CardHeader>
+                    <CardContent>
+                        <div className="h-[500px] w-full flex items-center">
+                            <Loader2 className="size-6  text-slate-300 animate-spin" />
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
+        )
+    }
 
     return (
         <div className="max-w-screen-4xl mx-auto w-full pb-10 -mt-24">
