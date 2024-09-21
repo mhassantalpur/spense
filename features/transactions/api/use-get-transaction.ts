4e92232f0093f33f956d2f41511701054111ec11
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-21 16:20:16 -0500

    feature: custom hook for get single transaction api endpoint

--- features/transactions/api/use-get-transaction.ts
+++ features/transactions/api/use-get-transaction.ts
@@ -1,16 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { client } from "@/lib/hono";
 
-export const useGetAccount= (id?:string) => {
+export const useGetTransaction = (id?:string) => {
     const query = useQuery({
         enabled: !!id, //only fetched if id exists
-        queryKey: ["account", { id }],
+        queryKey: ["transaction", { id }],
         queryFn: async () => {
-            const response = await client.api.accounts[":id"].$get({
+            const response = await client.api.transactions[":id"].$get({
                 param: {id},
             });
             if (!response.ok) {
-                throw new Error("Failed to fetch account");
+                throw new Error("Failed to fetch transaction");
             }
 
             const { data } = await response.json();
