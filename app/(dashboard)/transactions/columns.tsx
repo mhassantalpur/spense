95091bc02937d99b06b870a8cced9dfb5519f006
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-22 17:21:17 -0500

    feature: edit account from transactions

--- app/(dashboard)/transactions/columns.tsx
+++ app/(dashboard)/transactions/columns.tsx
@@ -10,6 +10,7 @@ import { Actions } from "./actions"
 import { format } from "date-fns"
 import { formatCurrency } from "@/lib/utils"
 import { Badge } from "@/components/ui/badge"
+import { AccountColumn } from "./account-column"
 
 export type ResponseType = InferResponseType<typeof client.api.transactions.$get, 200>["data"][0]
 
@@ -133,9 +134,10 @@ export const columns: ColumnDef<ResponseType>[] = [
     },
     cell: ({ row }) => {
       return (
-        <span>
-          {row.original.account}
-        </span>
+        <AccountColumn
+          account={row.original.account}
+          accountId={row.original.accountId}
+        />
       )
     }
   },
