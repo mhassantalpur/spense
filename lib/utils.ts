2252b37d18118c1b20a9d93c86ccac66a4f06be4
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-22 16:09:17 -0500

    feature: amount conversion function logic for db and client side

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// convert amount from db for client side
+export function converAmountFromMiliunits(amount: number) {
+  return amount / 1000;
+}
+
+// create amount to store integer value in db
+export function converAmountToMiliunits(amount: number) {
+  return Math.round(amount * 1000);
+}
