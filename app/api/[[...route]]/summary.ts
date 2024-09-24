e8ce4bbde11cb725c5fdb3582665137d628faff6
Hassan Talpur <hassantalpur22@gmail.com>
2024-09-24 17:15:53 -0500

    feature: summary api daily transactions summary calculation

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -103,7 +103,30 @@ const app = new Hono()
                     })
                 }
 
-            return c.json({ currentPeriod, lastPeriod, incomeChange, expensesChange, remainingChange, finalCategories })
+                // check summary of each transaction individually for the day
+                const activeDays = await db
+                    .select({
+                        date: transactions.date,
+                        income: sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+                        expenses: sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number)
+                    })
+                    .from(transactions)
+                    .innerJoin(
+                        accounts,
+                        eq(transactions.accountId, accounts.id)
+                    )
+                    .where(
+                        and(
+                            accountId ? eq(transactions.accountId, accountId) : undefined, 
+                            eq(accounts.userId, auth.userId),
+                            gte(transactions.date, startDate), 
+                            lte(transactions.date, endDate)
+                        )
+                    )
+                    .groupBy(transactions.date)
+                    .orderBy(transactions.date);
+                     
+            return c.json({ currentPeriod, lastPeriod, incomeChange, expensesChange, remainingChange, finalCategories, activeDays })
         },
     )
 
