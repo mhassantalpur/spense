6b2491bc2fb8667329638d6cfbe6c86b69b8fe61
Hassan Talpur <hassantalpur22@gmail.com>
2024-10-01 17:12:00 -0500

    feature: invalidate summary queries to refresh data on navigation to overview

--- features/accounts/api/use-bulk-delete-accounts.ts
+++ features/accounts/api/use-bulk-delete-accounts.ts
@@ -21,6 +21,7 @@ export const useBulkDeleteAccounts = () => {
         onSuccess: ()  => {
             toast.success("Accounts deleted");
             queryClient.invalidateQueries({ queryKey: ['accounts'] });
+            queryClient.invalidateQueries({ queryKey: ['summary'] });
         },
         onError: () => {
             toast.error("Failed to delete accounts")
