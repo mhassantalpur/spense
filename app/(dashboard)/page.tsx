dcfe38e09da98b81850250276039b9923cca372a
Hassan Talpur <hassantalpur22@gmail.com>
2024-10-01 18:28:06 -0500

    feature: summary chart data for income expenses with tooltip

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
+import { DataCharts } from "@/components/data-charts";
 import { DataGrid } from "@/components/data-grid";
 
 export default function DashboardPage() {
   return (
     <div className="text-black max-w-screen-4xl mx-auto w-full pb-10">
       <DataGrid />
+      <DataCharts />
     </div>
   )
 }
