f556334e0ac24163b0ee20ac97bae98703164a09
Hassan Talpur <hassantalpur22@gmail.com>
2024-10-01 14:01:27 -0500

    update: summary api last period calculation

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -54,7 +54,7 @@ const app = new Hono()
             };
 
             const [ currentPeriod ] = await fetchFinancialData(auth.userId, startDate, endDate);
-            const [ lastPeriod ] = await fetchFinancialData(auth.userId, startDate, endDate);
+            const [ lastPeriod ] = await fetchFinancialData(auth.userId, lastPeriodStart, lastPeriodEnd);
 
             const incomeChange = calculatePercentageChange(currentPeriod.income, lastPeriod.income);
             const expensesChange = calculatePercentageChange(currentPeriod.expenses, lastPeriod.expenses);
