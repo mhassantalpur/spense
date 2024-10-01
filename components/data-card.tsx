2ca1118395ebee0e0b00acb4eab98ff86878ad57
Hassan Talpur <hassantalpur22@gmail.com>
2024-10-01 16:32:05 -0500

    feature: skeleton loading conditional to overview cards

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -2,6 +2,7 @@ import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
 import { VariantProps, cva } from "class-variance-authority";
 import { IconType } from "react-icons";
 import { CountUp } from "@/components/count-up";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 const boxVariant = cva(
@@ -92,5 +93,23 @@ export const DataCard = ({
         </p> */}
       </CardContent>
 		</Card>
+  )
+}
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm h-[192px]">
+      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+        <div className="space-y-2">
+          <Skeleton className="h-6 w-24"/>
+          <Skeleton className="h-4 w-40"/>
+        </div>
+        <Skeleton className="size-12 "/>
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="shrink-0 h-10 w-24 mb-2"/>
+        <Skeleton className="shrink-0 h-4 w-40"/>
+      </CardContent>
+    </Card>
   )
 }
--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -4,18 +4,28 @@ import { useGetSummary } from "@/features/summary/api/use-get-summary";
 import { formatDateRange } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
 
-import { DataCard } from "@/components/data-card";
+import { DataCard, DataCardLoading } from "@/components/data-card";
 import { FaPiggyBank } from "react-icons/fa";
 import { FaArrowTrendUp, FaArrowTrendDown } from "react-icons/fa6";
 
 export const DataGrid = () => {
-	const { data } = useGetSummary();
+	const { data, isLoading } = useGetSummary();
 
 	const params = useSearchParams();
 	const to = params.get('to') || undefined;
 	const from = params.get('from') || undefined;
 
 	const dateRangeLabel = formatDateRange({ to, from }); // format date from params
+
+  if (isLoading) {
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
+        <DataCardLoading />
+        <DataCardLoading />
+        <DataCardLoading />
+      </div>
+    )
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
