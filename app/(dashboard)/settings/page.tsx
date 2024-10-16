fd086d7ec5f1a236ae515d52e8ea89a5cc31815c
Hassan Talpur <hassantalpur22@gmail.com>
2024-10-16 15:11:58 -0500

    feature: settings page skeleton

--- app/(dashboard)/settings/page.tsx
+++ app/(dashboard)/settings/page.tsx
@@ -1,7 +1,9 @@
+import { SettingsCard } from "./SettingsCard";
+
 const SettingsPage = () => {
   return (
     <div className="max-w-screen-4xl mx-auto w-full pb-10">
-      
+      <SettingsCard />
     </div>
   )
 }
