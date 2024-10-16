8f0ccfb24a0d55cb5046be1b0cafd7ab9c731bf3
Hassan Talpur <hassantalpur22@gmail.com>
2024-10-16 14:43:24 -0500

    feature: date filter param state

--- components/date-filter.tsx
+++ components/date-filter.tsx
@@ -18,7 +18,33 @@ import {
 } from "@/components/ui/popover"
 
 export const DateFilter = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const params = useSearchParams();
+  const accountId = params.get("accountId");
+  const from = params.get("from") || "";
+  const to = params.get("to") || "";
+
+  const defaultTo = new Date();
+  const defaultFrom = subDays(defaultTo, 30);
+
+  const paramState = {
+    from: from ? new Date(from) : defaultFrom,
+    to: to ? new Date(to) : defaultTo
+  }
+
   return (
-    <div>date-filter</div>
+    <Popover>
+      <PopoverTrigger>
+        <Button
+          disabled={false}
+          size="sm"
+          variant="outline"
+          className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition"
+        >
+          <span>{}</span>
+        </Button>
+      </PopoverTrigger>
+    </Popover>
   )
 }
