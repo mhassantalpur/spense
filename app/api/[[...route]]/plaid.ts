c8e224e985b853828e93a04ae67bbf39ba7356a6
Hassan Talpur <hassantalpur22@gmail.com>
2025-05-07 20:17:13 -0500

    feature: plaid link token api route configuration

--- app/api/[[...route]]/plaid.ts
+++ app/api/[[...route]]/plaid.ts
@@ -1,11 +1,40 @@
 import { Hono } from "hono";
-import { clerkMiddleware } from "@hono/clerk-auth";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { Configuration, CountryCode, PlaidApi, PlaidEnvironments, Products } from "plaid";
+
+const configuration = new Configuration({
+  basePath: PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_TOKEN,
+      "PLAID-SECRET": process.env.PLAID_SECRET_TOKEN
+    }
+  }
+})
+
+const client = new PlaidApi(configuration);
 
 const app = new Hono()
  .post("/create-link-token",
   clerkMiddleware(),
   async (c) => {
-    return c.json({ data: "token-exists" })
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+      return c.json({ error: "Unauthorized" }, 401)
+    }
+
+    const token = await client.linkTokenCreate({
+      user: {
+        client_user_id: auth.userId,
+      },
+      client_name: "Spense",
+      products: [Products.Transactions],
+      country_codes: [CountryCode.Us],
+      language: "en"
+    });
+
+    return c.json({ data: token.data })
   }
  )
 
